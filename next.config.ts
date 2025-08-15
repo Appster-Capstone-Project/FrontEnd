86ae2224a800afa621aff0d5867d6dd12c79a337
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-08-15 19:26:41 +0000

    Added next config

---
+++ next.config.ts
@@ -0,0 +1,77 @@
+
+import type {NextConfig} from 'next';
+import withPWA from 'next-pwa';
+
+const withPwa = withPWA({
+  dest: '.next/pwa',
+  register: true,
+  skipWaiting: true,
+  disable: false, // Ensure PWA is active in development
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */ // Add your existing Next.js configuration options here
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '52.255.203.119',
+        port: '9000',
+        pathname: '/**',
+      },
+       {
+        protocol: 'http',
+        hostname: '20.185.241.50',
+        port: '9000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '192.168.2.32',
+        port: '3000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '172.191.11.228',
+        port: '8000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.themanual.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      }
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        // This rewrite is for general API calls
+        source: '/api/:path*',
+        destination: 'http://172.191.11.228:8000/:path*', 
+      },
+    ];
+  },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: 'http://172.191.11.228:8000',
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000',
+  }
+};
+
+export default withPwa(nextConfig);
